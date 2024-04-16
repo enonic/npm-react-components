90d85b1fba9d5da4a56599ff55ecb4036aee276c
Christian Westgaard <comlock@gmail.com>
2024-04-16 10:58:50 +0200

    parseImageUrl handle custom url query params with multiple values

--- test/RichText/parseImageUrl.test.ts
+++ test/RichText/parseImageUrl.test.ts
@@ -7,7 +7,30 @@ import {parseImageUrl} from '../../src/RichText/parseImageUrl';
 
 
 describe('parseImageUrl', () => {
-	it('should parse an imageUrl', () => {
+	it('should parse a relative imageUrl', () => {
+		expect(parseImageUrl({
+			imageUrl: '/admin/site/preview/richproject/draft/_/image/e9b1f92b-fa46-4e58-b41f-87dc9f1999e8:9abf6cc6c7f565515175b33c08155b3495dcdf47/width-768/example.jpg'
+		})).toEqual({
+			'admin': 'admin',
+			'background': undefined,
+			'branch': 'draft',
+			'filename': 'example.jpg',
+			'filter': undefined,
+			'host': undefined,
+			'id': 'e9b1f92b-fa46-4e58-b41f-87dc9f1999e8',
+			'mode': 'preview',
+			'params': {},
+			'port': undefined,
+			'project': 'richproject',
+			'quality': undefined,
+			'scale': 'width(768)',
+			'scheme': undefined,
+			'type': undefined,
+			'versionKey': '9abf6cc6c7f565515175b33c08155b3495dcdf47',
+		})
+	});
+
+	it('should parse an absolute imageUrl', () => {
 		expect(parseImageUrl({
 			imageUrl: 'http://localhost:8080/admin/site/preview/richproject/draft/_/image/e9b1f92b-fa46-4e58-b41f-87dc9f1999e8:9abf6cc6c7f565515175b33c08155b3495dcdf47/width-768/example.jpg.png?background=ff0000&filter=rounded%285%29%3Bsharpen%28%29&quality=80'
 		})).toEqual({
@@ -27,6 +50,85 @@ describe('parseImageUrl', () => {
 			'scheme': 'http',
 			'type': 'absolute',
 			'versionKey': '9abf6cc6c7f565515175b33c08155b3495dcdf47',
-		})
+		});
+	});
+
+	it('should handle block scaling', () => {
+		expect(parseImageUrl({
+			imageUrl: '/admin/site/preview/richproject/draft/_/image/e9b1f92b-fa46-4e58-b41f-87dc9f1999e8:9abf6cc6c7f565515175b33c08155b3495dcdf47/block-1-2/example.jpg'
+		})).toEqual({
+			'admin': 'admin',
+			'background': undefined,
+			'branch': 'draft',
+			'filename': 'example.jpg',
+			'filter': undefined,
+			'host': undefined,
+			'id': 'e9b1f92b-fa46-4e58-b41f-87dc9f1999e8',
+			'mode': 'preview',
+			'params': {},
+			'port': undefined,
+			'project': 'richproject',
+			'quality': undefined,
+			'scale': 'block(1,2)',
+			'scheme': undefined,
+			'type': undefined,
+			'versionKey': '9abf6cc6c7f565515175b33c08155b3495dcdf47',
+		});
+	});
+
+	it('should handle full scaling', () => {
+		expect(parseImageUrl({
+			imageUrl: '/admin/site/preview/richproject/draft/_/image/e9b1f92b-fa46-4e58-b41f-87dc9f1999e8:9abf6cc6c7f565515175b33c08155b3495dcdf47/full/example.jpg'
+		})).toEqual({
+			'admin': 'admin',
+			'background': undefined,
+			'branch': 'draft',
+			'filename': 'example.jpg',
+			'filter': undefined,
+			'host': undefined,
+			'id': 'e9b1f92b-fa46-4e58-b41f-87dc9f1999e8',
+			'mode': 'preview',
+			'params': {},
+			'port': undefined,
+			'project': 'richproject',
+			'quality': undefined,
+			'scale': 'full',
+			'scheme': undefined,
+			'type': undefined,
+			'versionKey': '9abf6cc6c7f565515175b33c08155b3495dcdf47',
+		});
+	});
+
+	it('should handle custom url query params', () => {
+		expect(parseImageUrl({
+			imageUrl: '/admin/site/preview/richproject/draft/_/image/e9b1f92b-fa46-4e58-b41f-87dc9f1999e8:9abf6cc6c7f565515175b33c08155b3495dcdf47/height-300/example.jpg?justKey&key=value&sameKey=0&sameKey=1&sameKey=2'
+		})).toEqual({
+			'admin': 'admin',
+			'background': undefined,
+			'branch': 'draft',
+			'filename': 'example.jpg',
+			'filter': undefined,
+			'host': undefined,
+			'id': 'e9b1f92b-fa46-4e58-b41f-87dc9f1999e8',
+			'mode': 'preview',
+			'params': {
+				justKey: '',
+				key: 'value',
+				sameKey: ['0', '1', '2'],
+			},
+			'port': undefined,
+			'project': 'richproject',
+			'quality': undefined,
+			'scale': 'height(300)',
+			'scheme': undefined,
+			'type': undefined,
+			'versionKey': '9abf6cc6c7f565515175b33c08155b3495dcdf47',
+		});
+	});
+
+	it('should throw when no path in imageUrl', () => {
+		expect(() => parseImageUrl({
+			imageUrl: ''
+		})).toThrow('parseImageUrl: No path in imageUrl: !');
 	});
 });
