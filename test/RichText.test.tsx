efa9835253dc2ff9ba978141ff09e60560e50e9c
Christian Westgaard <comlock@gmail.com>
2024-04-18 13:47:50 +0200

    100% branch coverage on new code

--- test/RichText.test.tsx
+++ test/RichText.test.tsx
@@ -1,320 +1,49 @@
 import type {
-	ImageComponent,
-	ImageContent,
 	RichTextData,
 } from '../src/types';
 
 
 import {
-	beforeAll,
-	afterAll,
 	describe,
 	expect,
 	test as it
 } from '@jest/globals';
 import {render} from '@testing-library/react'
-import toDiffableHtml from 'diffable-html';
 import React from 'react';
-// import renderer from 'react-test-renderer';
 import {RichText} from '../src';
 import {Image} from './RichText/Image';
 import {Link} from './RichText/Link';
 import {Macro} from './RichText/Macro';
-// import {print} from 'q-i';
 
-
-const IMG_REF = '59b78b11-3abf-4b7e-b16e-a5b1e90efcb0';
-const IMG_ID = 'e9b1f92b-fa46-4e58-b41f-87dc9f1999e8'
-const IMG_VERSION_KEY = '9abf6cc6c7f565515175b33c08155b3495dcdf47';
-
-const IMAGE: ImageContent = {
-	_id: IMG_ID,
-	_name: 'example.jpg',
-	_path: '/mysite/example.jpg',
-	type: 'media:image',
-}
-
-const originalError = console.error
-beforeAll((done) => {
-	console.error = (...args) => {
-		// console.debug(args);
-		if (
-			args[0] === 'Warning: validateDOMNesting(...): %s cannot appear as a descendant of <%s>.%s'
-			&& args[1] === '<div>'
-			&& args[2] === 'p'
-		) {
-			return;
-		}
-		// console.debug(typeof args[0]);
-		if (
-			typeof args[0] === 'object'
-			&& args[0].detail instanceof Error
-			&& args[0].detail.message === 'Failed to render image!'
-		) {
-			// print(args[0], { maxItems: Infinity });
-			// console.debug(args[0]);
-			// console.debug(args[0].detail);
-			return;
-		}
-		console.debug(args[0].detail.message); // For some this line makes an error go away!?!
-		originalError(...args)
-	}
-	done();
-});
-
-afterAll((done) => {
-	console.error = originalError;
-	done();
-});
 
 describe('RichText', () => {
-	it('should remove data-image-ref from images', () => {
+	it('should support tag', () => {
 		const data: RichTextData = {
-			images: [{
-				image: IMAGE,
-				ref: IMG_REF,
-				// style: null
-			}],
+			images: [],
 			links: [],
 			macros: [],
-			processedHtml: `<figure class=\"captioned editor-align-right editor-width-custom\" style=\"float: right; width: 50%;\"><img alt=\"Alt text\" src=\"/admin/site/preview/richproject/draft/_/image/${IMG_ID}:${IMG_VERSION_KEY}/width-768/example.jpg\" style=\"width:100%\" data-image-ref=\"${IMG_REF}\">
-<figcaption>Caption</figcaption>
-</figure>`
+			processedHtml: `<p>Some text</p>`
 		}
 		const html = render(<RichText
-			className='myclass'
+			data={data}
+			Image={Image}
+			Link={Link}
+			Macro={Macro}
+			tag='article'
+		/>).baseElement;
+		expect(html.outerHTML).toBe(`<body><div><article><p>Some text</p></article></div></body>`);
+	});
+
+	it('should return an empty element when processedHtml is missing or empty', () => {
+		// @ts-expect-error
+		const data: RichTextData = {};
+
+		const html = render(<RichText
 			data={data}
 			Image={Image}
 			Link={Link}
 			Macro={Macro}
 		/>).baseElement;
-		// print(html.outerHTML, { maxItems: Infinity });
-		expect(html.outerHTML).toBe(`<body><div><section class="myclass"><figure class="captioned editor-align-right editor-width-custom" style="float: right; width: 50%;"><img alt="Alt text" src="/admin/site/preview/richproject/draft/_/image/${IMG_ID}:${IMG_VERSION_KEY}/width-768/example.jpg" style="width: 100%;">
-<figcaption>Caption</figcaption>
-</figure></section></div></body>`);
-	});
-
-	it('should show an ErrorComponent when Image component throws', () => {
-		const ImageThatThrows: ImageComponent = () => {
-			throw new Error('Failed to render image!');
-		};
-		const data: RichTextData = {
-			images: [{
-				image: IMAGE,
-				ref: IMG_REF,
-				// style: null
-			}],
-			links: [],
-			macros: [],
-			processedHtml: `<figure class=\"captioned editor-align-right editor-width-custom\" style=\"float: right; width: 50%;\"><img alt=\"Alt text\" src=\"/admin/site/preview/richproject/draft/_/image/${IMG_ID}:${IMG_VERSION_KEY}/width-768/example.jpg\" style=\"width:100%\" data-image-ref=\"${IMG_REF}\">
-<figcaption>Caption</figcaption>
-</figure>`
-		}
-		const html = render(<RichText
-			className='myclass'
-			data={data}
-			Image={ImageThatThrows}
-			Link={Link}
-			Macro={Macro}
-		/>).baseElement;
-		// print(html.outerHTML, { maxItems: Infinity });
-		expect(html.outerHTML).toBe(`<body><div><section class="myclass"><figure class="captioned editor-align-right editor-width-custom" style="float: right; width: 50%;"><div style="border: 1px dotted red; color: red;">Failed to render image!</div>
-<figcaption>Caption</figcaption>
-</figure></section></div></body>`);
-	});
-
-	it('should remove data-image-ref from images with srcsets', () => {
-		const dataWithSrcSet: RichTextData = {
-			images: [{
-				image: IMAGE,
-				ref: IMG_REF,
-				// style: null
-			}],
-			links: [],
-			macros: [],
-			processedHtml: `<figure class=\"captioned editor-align-right editor-width-custom\" style=\"float: right; width: 50%;\"><img alt=\"Alt text\" src=\"/admin/site/preview/richproject/draft/_/image/${IMG_ID}:${IMG_VERSION_KEY}/width-768/example.jpg\" style=\"width:100%\" srcset=\"/admin/site/preview/richproject/draft/_/image/${IMG_ID}:${IMG_VERSION_KEY}/width-2048/example.jpg 2048w,/admin/site/preview/richproject/draft/_/image/${IMG_ID}:${IMG_VERSION_KEY}/width-1024/example.jpg 1024w\" sizes=\"juhu\" data-image-ref=\"${IMG_REF}\">\n<figcaption>Caption</figcaption>\n</figure>`
-		}
-		const html = render(<RichText
-			className='myclass'
-			data={dataWithSrcSet}
-			Image={Image}
-			Link={Link}
-			Macro={Macro}
-		/>).baseElement;
-		// print(html.outerHTML, { maxItems: Infinity });
-		expect(html.outerHTML).toBe(`<body><div><section class="myclass"><figure class="captioned editor-align-right editor-width-custom" style="float: right; width: 50%;"><img alt="Alt text" sizes="juhu" src="/admin/site/preview/richproject/draft/_/image/${IMG_ID}:${IMG_VERSION_KEY}/width-768/example.jpg" srcset="/admin/site/preview/richproject/draft/_/image/${IMG_ID}:${IMG_VERSION_KEY}/width-2048/example.jpg 2048w,/admin/site/preview/richproject/draft/_/image/${IMG_ID}:${IMG_VERSION_KEY}/width-1024/example.jpg 1024w" style="width: 100%;">
-<figcaption>Caption</figcaption>
-</figure></section></div></body>`);
-	});
-
-	it('should handle image without style attribute', () => {
-		const dataWithSrcSet: RichTextData = {
-			images: [{
-				image: IMAGE,
-				ref: IMG_REF,
-			}],
-			links: [],
-			macros: [],
-			processedHtml: `<figure class=\"captioned editor-align-right editor-width-custom\" style=\"float: right; width: 50%;\">
-	<img
-		alt=\"Alt text\"
-		data-image-ref=\"${IMG_REF}\"
-		src=\"/admin/site/preview/richproject/draft/_/image/${IMG_ID}:${IMG_VERSION_KEY}/width-768/example.jpg\"
-	>
-	<figcaption>Caption</figcaption>
-</figure>`
-		}
-		const html = render(<RichText
-			className='myclass'
-			data={dataWithSrcSet}
-			Image={Image}
-			Link={Link}
-			Macro={Macro}
-		/>).baseElement;
-		// print(html.outerHTML, { maxItems: Infinity });
-		expect(toDiffableHtml(html.outerHTML)).toBe(`
-<body>
-  <div>
-    <section class="myclass">
-      <figure
-        class="captioned editor-align-right editor-width-custom"
-        style="float: right; width: 50%;"
-      >
-        <img
-          alt="Alt text"
-          src="/admin/site/preview/richproject/draft/_/image/${IMG_ID}:${IMG_VERSION_KEY}/width-768/example.jpg"
-        >
-        <figcaption>
-          Caption
-        </figcaption>
-      </figure>
-    </section>
-  </div>
-</body>
-`);
-	});
-
-	it('should show an ErrorComponent when images object is missing', () => {
-		const ImageThatThrows: ImageComponent = () => {
-			throw new Error('Failed to render image!');
-		};
-		const data: RichTextData = {
-			// images: [],
-			processedHtml: `<figure class=\"captioned editor-align-right editor-width-custom\" style=\"float: right; width: 50%;\"><img alt=\"Alt text\" src=\"/admin/site/preview/richproject/draft/_/image/${IMG_ID}:${IMG_VERSION_KEY}/width-768/example.jpg\" style=\"width:100%\" data-image-ref=\"${IMG_REF}\">
-<figcaption>Caption</figcaption>
-</figure>`
-		}
-		const html = render(<RichText
-			className='myclass'
-			data={data}
-			Image={ImageThatThrows}
-			Link={Link}
-			Macro={Macro}
-		/>).baseElement;
-		// print(html.outerHTML, { maxItems: Infinity });
-		expect(toDiffableHtml(html.outerHTML)).toBe(`
-<body>
-  <div>
-    <section class="myclass">
-      <figure
-        class="captioned editor-align-right editor-width-custom"
-        style="float: right; width: 50%;"
-      >
-        <div style="border: 1px dotted red; color: red;">
-          Can't replace image, when there are no images in the data object!
-        </div>
-        <figcaption>
-          Caption
-        </figcaption>
-      </figure>
-    </section>
-  </div>
-</body>
-`);
-	});
-
-	it('should show an ErrorComponent when image element is missing data-image-ref attribute', () => {
-		const ImageThatThrows: ImageComponent = () => {
-			throw new Error('Failed to render image!');
-		};
-		const data: RichTextData = {
-			images: [{
-				image: IMAGE,
-				ref: 'whatever',
-			}],
-			processedHtml: `<figure class=\"captioned editor-align-right editor-width-custom\" style=\"float: right; width: 50%;\">
-	<img alt=\"Alt text\" src=\"/admin/site/preview/richproject/draft/_/image/${IMG_ID}:${IMG_VERSION_KEY}/width-768/example.jpg\" style=\"width:100%\">
-	<figcaption>Caption</figcaption>
-</figure>`
-		}
-		const html = render(<RichText
-			className='myclass'
-			data={data}
-			Image={ImageThatThrows}
-			Link={Link}
-			Macro={Macro}
-		/>).baseElement;
-		// print(html.outerHTML, { maxItems: Infinity });
-		expect(toDiffableHtml(html.outerHTML)).toBe(`
-<body>
-  <div>
-    <section class="myclass">
-      <figure
-        class="captioned editor-align-right editor-width-custom"
-        style="float: right; width: 50%;"
-      >
-        <div style="border: 1px dotted red; color: red;">
-          Image element has no data-image-ref attibute!
-        </div>
-        <figcaption>
-          Caption
-        </figcaption>
-      </figure>
-    </section>
-  </div>
-</body>
-`);
-	});
-
-	it('should show an ErrorComponent when image not found in image', () => {
-		const ImageThatThrows: ImageComponent = () => {
-			throw new Error('Failed to render image!');
-		};
-		const data: RichTextData = {
-			images: [{
-				image: IMAGE,
-				ref: 'wrongRef',
-			}],
-			processedHtml: `<figure class=\"captioned editor-align-right editor-width-custom\" style=\"float: right; width: 50%;\"><img alt=\"Alt text\" src=\"/admin/site/preview/richproject/draft/_/image/${IMG_ID}:${IMG_VERSION_KEY}/width-768/example.jpg\" style=\"width:100%\" data-image-ref=\"${IMG_REF}\">
-<figcaption>Caption</figcaption>
-</figure>`
-		}
-		const html = render(<RichText
-			className='myclass'
-			data={data}
-			Image={ImageThatThrows}
-			Link={Link}
-			Macro={Macro}
-		/>).baseElement;
-		// print(html.outerHTML, { maxItems: Infinity });
-		expect(toDiffableHtml(html.outerHTML)).toBe(`
-<body>
-  <div>
-    <section class="myclass">
-      <figure
-        class="captioned editor-align-right editor-width-custom"
-        style="float: right; width: 50%;"
-      >
-        <div style="border: 1px dotted red; color: red;">
-          Unable to find image with ref ${IMG_REF} in images object!
-        </div>
-        <figcaption>
-          Caption
-        </figcaption>
-      </figure>
-    </section>
-  </div>
-</body>
-`);
+		expect(html.outerHTML).toBe(`<body><div><section></section></div></body>`);
 	});
 }); // describe RichText
