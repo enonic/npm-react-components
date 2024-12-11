0456daf7a43dd1439ceff980dfa9f7754195a5f9
Christian Westgaard <comlock@gmail.com>
2024-12-11 10:01:10 +0100

    Move all dependencies to devDependencies since they are all bundled

--- tsup.config.ts
+++ tsup.config.ts
@@ -18,10 +18,20 @@ const ENTRY = [
 const external = [];
 
 const noExternal = [
-	'@enonic/js-utils',
+	'@enonic/js-utils', // We only use toStr which is so small, might as well just bundle it.
+
+	// Maybe these could remain dependencies in package.json,
+	// but currently that seems to cause runtime errors with starter-react4xp.
+	'clsx',
 	'domelementtype',
-	// 'html-dom-parser',
+
+	// The entities node module contains Uint16Array which fails in Nashorn.
+	// html-react-parser has multiple layers of dependencies that ends up dragging in entities.
+	// html-react-parser -> html-dom-parser -> htmlparser2 -> domutils -> dom-serializer -> entities
+	// To avoid the problem we bundle the server variant of html-dom-parser which does not depend on entities.
+	// As such is should be kept a devDependency in the package.json and be listed as an external here:
 	'html-react-parser',
+
 	'react', // For GlobalsPlugin to work react MUST be listed here (if react under dependencies or peerDependencies)
 ];
 
@@ -40,7 +50,6 @@ export default defineConfig((options: MyOptions) => {
 			entry: ENTRY,
 			esbuildOptions(options) {
 				options.alias = {
-					// "html-dom-parser": "./node_modules/html-dom-parser/esm/server/html-to-dom.mjs",
 					"html-dom-parser": "./node_modules/html-dom-parser/lib/server/html-to-dom.js",
 				}
 			},
@@ -63,7 +72,6 @@ export default defineConfig((options: MyOptions) => {
 			esbuildOptions(options) {
 				options.alias = {
 					"html-dom-parser": "./node_modules/html-dom-parser/esm/server/html-to-dom.mjs",
-					// "html-dom-parser": "./node_modules/html-dom-parser/lib/server/html-to-dom.js",
 				}
 			},
 			esbuildPlugins,
