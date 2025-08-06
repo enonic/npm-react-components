bfe7d3c5bdfb7e9c25ad038f229dab6db4cf00fc
Pavel Milkevich <pmi@enonic.com>
2025-08-06 14:05:39 +0200

    Create initPublicPath file to set webpack's publicPath automatically #230

---
+++ src/utils/initPublicPath.ts
@@ -0,0 +1,5 @@
+// @ts-ignore __R4XP_PUBLIC_PATH__ value will be set by react4xp
+if (typeof window !== 'undefined' && window.__R4XP_PUBLIC_PATH__) {
+    // @ts-ignore webpack needs this EXACT line to set the public path correctly
+    __webpack_public_path__ = window.__R4XP_PUBLIC_PATH__;
+}
